3881adab7713862593a6897d67a68a700a46f639
Manuel Duran <mduran85@gmail.com>
2020-02-25 16:14:32 -0600

    removed data

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const router = require('./router');
 var cors = require('cors')
 const mongoose = require('mongoose');
 const db = require('../database/index.js');
-const ID = process.env.ID || 'manuel:IgkOZwxDNwyDOwfJP';
+const ID = process.env.ID;
 
 mongoose.connect(host=`mongodb://${ID}@ec2-52-36-229-171.us-west-2.compute.amazonaws.com:80/pictures`, {useNewUrlParser: true})
 .then(() => {
